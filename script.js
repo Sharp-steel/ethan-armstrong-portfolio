2a6cb691fd47ecedbc21f39807ab4745930f83eb
Sharp-steel <ethan.arm13@gmail.com>
2026-09-03 17:44:27 -0400

    Added mobile menu

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMobileMenu() {
+    const menu = document.querySelector('.mobile-menu')
+    menu.style.display = 'flex'
+}
+
+function hideMobileMenu() {
+    const x = document.querySelector('.mobile-menu')
+    x.style.display = 'none'
+}
